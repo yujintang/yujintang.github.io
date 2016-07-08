671983763b96b2013cbc80a16a4a20d00a8667b3
余金堂 <shanquan54@163.com>
2016-07-08 13:24:48 +0800

    add

--- push_hexo_branch.sh
+++ push_hexo_branch.sh
@@ -2,10 +2,22 @@
 
 echo 'start git push origin hexo ......'
 
+comment="add_article"
+if(!-n"$1")
+then
+comment="add_article"
+echo "++++"
+else
+echo "-----"
+comment=$1
+fi
+echo "_______"
+echo $comment
+
 hexo clean
 git checkout hexo
 git add .
-git commit -m $1
+git commit -m ${comment}
 git push origin hexo
 
 echo 'finished'
